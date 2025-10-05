8c5b5d05de74d800797ae02abcbfc2ff59b9e482
kush150 <kushpandey26@gmail.com>
2025-10-05 15:34:37 +0530

    retry

---
+++ instance/Instance_variable.tf
@@ -0,0 +1,12 @@
+# This is the list of variable usig in instance creation 
+variable "my_ami" {
+}
+variable "instance_type" {
+
+}
+variable "key_name" {
+ }
+variable "region" {
+}   
+
+# Adding variable for instance 
